7de92112a41ea42ca777109cfa908213063f6b49
shobit singh <38094214+Shobit123@users.noreply.github.com>
2021-06-10 02:18:53 +0530

    Update dbConnect.php

--- dbConnect.php
+++ dbConnect.php
@@ -1,7 +1,7 @@
 <?php  
 require_once 'config.php';
 
-$db = new mysqli("oxygenakaunting.cluster-cladr7eisf0t.us-east-1.rds.amazonaws.com", "VishalK", "n1UzKuIBIYAjxoq4","akaunting");
+$db = new mysqli("localhost", "auth", "root"," ");
 
 if ($db->connect_errno) {  
     printf("Connect failed: %s\n", $db->connect_error);  
